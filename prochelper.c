d75a98d5694c5f4cecf38b1beb65d58a044ab6da
SteliosMalamas <malamas_paros02@yahoo.com>
2022-07-27 14:49:40 +0300

    Color are now brighter

--- prochelper.c
+++ prochelper.c
@@ -13,12 +13,12 @@
 #include <stdint.h>
 #include <errno.h>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RED     "\x1b[31;1m"
+#define ANSI_COLOR_GREEN   "\x1b[32;1m"
+#define ANSI_COLOR_YELLOW  "\x1b[33;1m"
+#define ANSI_COLOR_BLUE    "\x1b[34;1m"
+#define ANSI_COLOR_MAGENTA "\x1b[35;1m"
+#define ANSI_COLOR_CYAN    "\x1b[34;1m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 
